a3d1046357d02ab28635938cb056c34242b8e409
Ilya <37378410+Std1o@users.noreply.github.com>
2022-09-28 14:49:18 +0300

    upd ContactsListAdapter

--- app/src/main/java/com/ermakov/roomsample/presentation/ui/adapter/ContactsListAdapter.kt
+++ app/src/main/java/com/ermakov/roomsample/presentation/ui/adapter/ContactsListAdapter.kt
@@ -1,5 +1,6 @@
 package com.ermakov.roomsample.presentation.ui.adapter
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,29 +11,29 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ermakov.roomsample.R
 import com.ermakov.roomsample.domain.model.Contact
 
-class ContactsListAdapter : ListAdapter<Contact, ContactsListAdapter.WordViewHolder>(WordsComparator()) {
+class ContactsListAdapter : ListAdapter<Contact, ContactsListAdapter.ViewHolder>(WordsComparator()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
-        return WordViewHolder.create(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.create(parent)
     }
 
-    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val current = getItem(position)
         holder.bind(current.name, current.phone)
     }
 
-    class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val wordItemView: TextView = itemView.findViewById(R.id.textView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val textView: TextView = itemView.findViewById(R.id.textView)
 
         fun bind(name: String, phone: String) {
-            wordItemView.text = "$name\n$phone"
+            textView.text = itemView.context.getString(R.string.item_contact, name, phone)
         }
 
         companion object {
-            fun create(parent: ViewGroup): WordViewHolder {
+            fun create(parent: ViewGroup): ViewHolder {
                 val view: View = LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_word, parent, false)
-                return WordViewHolder(view)
+                    .inflate(R.layout.item_contact, parent, false)
+                return ViewHolder(view)
             }
         }
     }
